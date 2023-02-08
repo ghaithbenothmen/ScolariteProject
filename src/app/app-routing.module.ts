d1bd12030147f7b33869ba38f9bdbe577768b21a
Ben Othmen Ghaith <ghaithbenothmen8@gmail.com>
2023-02-08 01:13:37 +0100

    saving github

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ApprenantComponent } from './apprenant/apprenant.component';
+const routes: Routes = [
+  
+  {path:"login",component:LoginComponent},
+  {path:"apprenant",component:ApprenantComponent},
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/apprenant/apprenant.component.ts
+++ src/app/apprenant/apprenant.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-apprenant',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./apprenant.component.css']
 })
 export class ApprenantComponent {
+[x: string]: any;
+public modalRef!: BsModalRef;
+
+constructor(private modalService: BsModalService) {}
+
+openModal(modalTemplate: TemplateRef<any>) {
+  this.modalRef = this.modalService.show(modalTemplate,
+    {
+      class: 'modal-dialogue-centered modal-md',
+      backdrop: 'static',
+      keyboard: true
+    }
+  );
+}
+
 
 }
